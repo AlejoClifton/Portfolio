88efef0a700f18ae9b58ffe14c821f7ebee477e9
Alejo Tomás Clifton Goldney <cliftonalejo@gmail.com>
2022-01-15 00:20:45 -0300

    Portfolio 1.4

--- src/views/NavBar/ListNavBar.js
+++ src/views/NavBar/ListNavBar.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NavBarCategory from './NavBarCategory';
 
 const ListNavBar = ({ listCategories }) => {
+    const [estado, setEstado] = useState(false);
+    const [clase, setClase] = useState('');
+    const menuButton = () => {
+        if (!estado) {
+            setClase('scale-up-ver-top public');
+            setEstado(true);
+        } else {
+            setClase('');
+            setEstado(false);
+        }
+    };
+
     return (
         <div className="aside">
             <h1 className="title4">
                 <a href="/">Alejo</a>
             </h1>
-            <ul>
+            <ul className={clase}>
                 {listCategories.map((category) => {
                     return <NavBarCategory key={category.id} category={category} />;
                 })}
             </ul>
+            <i onClick={menuButton} className="fas fa-bars"></i>
         </div>
     );
 };
